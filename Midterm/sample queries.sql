3a74d073afa7308e852474f042a663107dc255b8
Relatedly <146494485+Relatedly@users.noreply.github.com>
2025-03-14 15:36:12 -0400

    Create sample queries.sql

---
+++ Midterm/sample queries.sql
@@ -0,0 +1,15 @@
+SELECT * FROM Book;
+SELECT * FROM Customer WHERE email = 'jane.doe@example.com';
+
+SELECT Order.order_id, Customer.first_name, Customer.last_name, Book.title, OrderItem.quantity
+FROM Order
+JOIN Customer ON Order.customer_id = Customer.customer_id
+JOIN OrderItem ON Order.order_id = OrderItem.order_id
+JOIN Book ON OrderItem.book_id = Book.book_id;
+
+SELECT genre, AVG(price) AS average_price
+FROM Book
+GROUP BY genre;
+
+SELECT COUNT(order_id) AS total_orders
+FROM Order;

d1d26bede5ddd13cd0cfea73c0046a5c48986992
Relatedly <146494485+Relatedly@users.noreply.github.com>
2025-03-24 23:09:29 -0400

    Update and rename sample queries.sql to Bookstore Queries.sql

--- Midterm/Bookstore Queries.sql
+++ Midterm/Bookstore Queries.sql
@@ -1,8 +1,8 @@
 SELECT * FROM Book;
 SELECT * FROM Customer WHERE email = 'cbickerdickeq@apple.com';
 
-SELECT Order.order_id, Customer.first_name, Customer.last_name, Book.title, OrderItem.quantity
-FROM Order
+SELECT Orders.order_id, Customer.first_name, Customer.last_name, Book.title, OrderItem.quantity
+FROM Orders
 JOIN Customer ON Order.customer_id = Customer.customer_id
 JOIN OrderItem ON Order.order_id = OrderItem.order_id
 JOIN Book ON OrderItem.book_id = Book.book_id;
@@ -12,4 +12,4 @@ FROM Book
 GROUP BY genre;
 
 SELECT COUNT(order_id) AS total_orders
-FROM Order;
+FROM Orders;
